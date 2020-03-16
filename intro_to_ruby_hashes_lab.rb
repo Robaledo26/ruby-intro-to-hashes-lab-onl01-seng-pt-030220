daab7388b0ec5e64319de99370062b3917af39da
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-16 00:09:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,11 +23,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	Monopoly = {:railroads=>
-	{
-	:piences=>4
-	:names=>
-	{
+
 	:pieces=>4,
 		:names=>
 		{
@@ -45,7 +41,6 @@ def monopoly_with_third_tier
 		}
 		}
 	}
-
 end
 
 def monopoly_with_fourth_tier
